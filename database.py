307a27d10541916ebc2571f3d574189372562772
Tymek <tradz06@outlook.com>
2024-09-24 18:57:18 +0200

    added password hashing and database testing program

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
-from os.path import curdir
-
 import mysql.connector
+import time
 
 
 class Database:
@@ -27,7 +26,7 @@ class Database:
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS user(
             id int PRIMARY KEY AUTO_INCREMENT,
             username varchar(50),
-            password varchar(100)
+            password varchar(256)
             )
             ''')
 
@@ -46,7 +45,7 @@ class Database:
 
         self.cursor.execute(f"SELECT * FROM user WHERE username LIKE '{username}'")
         if self.cursor.rowcount == 0:
-            self.cursor.execute("INSERT INTO user VALUES (NULL, %s, %s)",
+            self.cursor.execute("INSERT INTO user VALUES (NULL, %s, SHA2(%s, 256))",
                                 (username, password))
             self.db.commit()
             return "success"
@@ -60,22 +59,22 @@ class Database:
     def login(self, username, password):
         self.create_database()
 
-        query = self.cursor.execute("SELECT * FROM user WHERE username = %s AND password = %s",
-                                    (username, password))
-        return query
+        self.cursor.execute("SELECT * FROM user WHERE username = %s AND password = SHA2(%s, 256)",
+                            (username, password))
+        return self.cursor.fetchone()
 
-    def create_note(self, user_id, content, date):
+    def create_note(self, user_id, content):
         self.create_database()
 
-        self.cursor.execute("INSERT INTO note VALUES (NULL, %s, %s, %s)",
-                            (user_id, content, date))
+        self.cursor.execute("INSERT INTO note VALUES (NULL, %s, %s, NULL)",
+                            (user_id, content))
         self.db.commit()
 
-    def edit_note(self, note_id, content, date):
+    def edit_note(self, note_id, content):
         self.create_database()
 
-        self.cursor.execute("UPDATE note SET content = %s, date = %s WHERE id = %s",
-                            (content, date, note_id))
+        self.cursor.execute("UPDATE note SET content = %s, date = NULL WHERE id = %s",
+                            (content, note_id))
         self.db.commit()
 
     def delete_note(self, note_id):
@@ -85,33 +84,113 @@ class Database:
     def select_all_notes(self, user_id):
         self.create_database()
 
-        query = self.cursor.execute(f"SELECT * FROM note WHERE user_id LIKE '{user_id}'")
-        return query
+        self.cursor.execute(f"SELECT * FROM note WHERE user_id LIKE '{user_id}'")
+        return self.cursor.fetchall()
 
     def select_notes_by_date(self, user_id, date):
         self.create_database()
 
-        query = self.cursor.execute("SELECT * FROM note WHERE user_id LIKE '%s' AND date = %s",
-                                    (user_id, date))
-        return query
+        self.cursor.execute("SELECT * FROM note WHERE user_id LIKE '%s' AND date = %s",
+                            (user_id, date))
+        return self.cursor.fetchall()
 
 
 if __name__ == '__main__':
-    baza = Database('notepad')
+    Base = Database('notepad')
 
-    un = "kamil"
-    pw = "dda"
+    user_id = None
+    user_username = None
+    user_password = None
 
-    # creating account
-    # query_create = baza.create_user(un, pw)
-    # if  query_create == "success":
-    #    print("user created")
-    # elif query_create == "username_taken":
-    #    print("user alredy exists")
+    while True:
+        print("+------------------------+-----------------------+-----------------------+\n"
+              "| 1 - opcje użytkownika  |   2 - opcje notatek   |      0 - zamknij      |\n"
+              "+------------------------+-----------------------+-----------------------+")
+        i = int(input("Wybierz opcję: "))
 
-    # login
-    # q = baza.login(un, pw)
-    # if q is None:
-    #    print("Nie mo")
-    # else:
-    #    print("logged")
+        if i == 1:
+            while True:
+                print(
+                    "+--------------------+--------------------+--------------------+--------------------+\n"
+                    "|  1 - utwórz konto  |  2 - zaloguj się   |   3 - usuń konto   |     0 - powrót     |\n"
+                    "+--------------------+--------------------+--------------------+--------------------+")
+                u = int(input("Wybierz opcję: "))
+
+                if u == 1:
+                    un = input("Podaj login:")
+                    pw = input("Podaj hasło:")
+                    query_create = Base.create_user(un, pw)
+                    if query_create == "success":
+                        print("Utworzono konto")
+                    elif query_create == "username_taken":
+                        print("Konto o podanym loginie istnieje")
+
+                elif u == 2:
+                    un = input("Podaj login:")
+                    pw = input("Podaj hasło:")
+                    query_login = Base.login(un, pw)
+                    if query_login is None:
+                        print("Błędne dane logowawnia")
+                    else:
+                        user_id = query_login[0]
+                        user_username = query_login[1]
+                        user_password = query_login[2]
+                        print("Zalogowano pomyślnie")
+                        break
+                elif u == 3:
+                    if user_id is None:
+                        print("Nie zalogowano")
+                    else:
+                        Base.delete_user(user_id)
+                        print("Usunięto konto")
+                elif u == 0:
+                    break
+                else:
+                    print("Nieprawidłowe dane")
+        elif i == 2:
+            while True:
+                print(
+                    "+------------------+------------------+------------------+------------------+------------------+\n"
+                    "| 1 - nowa notatka |   2 - edytuj     |   3 - wyświetl   |     4 - usuń     |    0 - powrót    |\n"
+                    "+------------------+------------------+------------------+------------------+------------------+")
+                n = int(input("Wybierz opcję: "))
+
+                if n == 1:
+                    if user_id is None:
+                        print("Najpierw się zaloguj")
+                        break
+                    else:
+                        content = input("Wpisz treść notatki:")
+                        date = time.time()
+                        Base.create_note(user_id, content)
+                        print("Utworzono notatkę")
+
+                elif n == 2:
+                    note_id = input("Podaj id notatki:")
+                    new_content = input("Wpisz nową treść notatki:")
+                    date = time.time()
+                    Base.edit_note(note_id, new_content)
+                    print("Edytowano notatkę")
+                elif n == 3:
+                    if user_id is None:
+                        print("Najpierw się zaloguj")
+                        break
+                    else:
+                        select_query = Base.select_all_notes(user_id)
+                        if len(select_query) == 0:
+                            print("Brak notatek")
+                        else:
+                            for row in select_query:
+                                date = str(row[3].strftime("%d.%m.%Y, %H:%M"))
+                                print(f"{row[2]} | {date}")
+                elif n == 4:
+                    note_id = int(input("Podaj id notatki:"))
+                    Base.delete_note(note_id)
+                elif n == 0:
+                    break
+                else:
+                    print("Nieprawidłowe dane")
+        elif i == 0:
+            break
+        else:
+            print("Nieprawidłowe dane")
